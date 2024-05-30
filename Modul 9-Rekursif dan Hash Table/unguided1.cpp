91ec4b9e4075b495eeda97cbab2b3bb31aaf0c99
hinaila <161398108+hinaila@users.noreply.github.com>
2024-05-31 00:04:35 +0700

    Add files via upload

---
+++ Modul 9-Rekursif dan Hash Table/unguided1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int faktorial(int n) {
+    if (n == 1)
+        return 1;
+    else
+        return n * faktorial(n - 1);
+}
+
+int main() {
+    int bilangan;
+    cout << "Masukkan bilangan bulat positif: ";
+    cin >> bilangan;
+
+    if (bilangan < 0) {
+        cout << "Masukkan bilangan bulat positif!" << endl;
+    } else {
+        int hasil = faktorial(bilangan);
+        cout << "Faktorial dari " << bilangan << " adalah: " << hasil << endl;
+    }
+
+    return 0;
+}
